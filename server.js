40b7e940d0cc98be50f89745f4f137ec178856c2
Tyler <realpsych22@gmail.com>
2020-04-02 22:32:33 -0500

    Protect against MIME and XSS

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require("helmet");
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+
 
 var app = express();
 
@@ -17,6 +19,8 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
 
 //Index page (static HTML)
 app.route('/')
